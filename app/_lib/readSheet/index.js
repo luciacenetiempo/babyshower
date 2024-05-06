df24d16cea8e0b5b9a8282602b54619670518226
Lucia <38978846+luciacenetiempo@users.noreply.github.com>
2024-05-06 16:38:04 +0200

    c

--- app/_lib/readSheet/index.js
+++ app/_lib/readSheet/index.js
@@ -19,7 +19,7 @@ export const getSheetsData = async (nomecognome) => {
     const date = function getFormattedDate() {
         const now = new Date();
         const day = now.getDate().toString().padStart(2, '0');
-        const month = (now.getMonth() + 1).toString().padStart(2, '0'); // getMonth() ritorna il mese da 0 (gennaio) a 11 (dicembre)
+        const month = (now.getMonth() + 1).toString().padStart(2, '0'); 
         const year = now.getFullYear();
         const hours = now.getHours().toString().padStart(2, '0');
         const minutes = now.getMinutes().toString().padStart(2, '0');
